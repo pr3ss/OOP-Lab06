a0104189085c666a2e4b576778ea9b7122656fdc
alex presepi <alex.presepi@studio.unibo.it>
2021-11-02 18:02:26 +0100

    essage=Change Robot for work with throw

--- src/it/unibo/oop/lab/exception1/Robot.java
+++ src/it/unibo/oop/lab/exception1/Robot.java
@@ -86,14 +86,23 @@ public class Robot {
     private boolean moveToPosition(final int newX, final int newY) {
         boolean returnValue = true;
         if (this.isBatteryEnoughToMove()) {
-            if (this.environment.move(newX, newY)) {
+        	try {
+        		this.environment.move(newX, newY);
+        		this.consumeBatteryForMovement();
+                this.log("Moved to position(" + newX + "," + newY + ").");
+        	}catch(PositionOutOfBoundException e){
+        		System.out.println(e.toString());
+        		returnValue=false;
+        	}
+        	
+            /*if (this.environment.move(newX, newY)) {
                 this.consumeBatteryForMovement();
                 this.log("Moved to position(" + newX + "," + newY + ").");
             } else {
                 this.log("Can not move to (" + newX + "," + newY
                         + ") the robot is touching at least one world boundary");
                 returnValue = false;
-            }
+            }*/
         } else {
             this.log("Can not move to position(" + newX + "," + newY + "). Not enough battery.");
             returnValue = false;
